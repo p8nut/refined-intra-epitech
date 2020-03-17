835f48a258169d47c995eaf8a9d22727dc278f35
andrey_j <jeremy.andrey.90@gmail.com>
2020-03-17 10:13:44 +0100

    [FIX] in activities sometime the button don't showup

--- scripts/activity.js
+++ scripts/activity.js
@@ -4,20 +4,25 @@
 
         const list_inscrit_table = document.getElementById('list-inscrit');
         let button = null
-        // Create an observer instance linked to the callback function
-        const observer = new MutationObserver((mutationsList, observer) => {
+        function upsertTeamsButton() {
             const buttons_container = Array.from(document.getElementsByClassName('admin')).find((e, id) => id === 0);
             if (!buttons_container) return;
             const students = Array.from(list_inscrit_table.getElementsByTagName('td')).filter((e) => e.dataset.login).map((e) => e.dataset.login)
             if (students.length === 0) return;
             if (button !== null) {
-                buttons_container.removeChild(button)    
+                buttons_container.removeChild(button)
             }
             button = createButtonTeams(students)
             buttons_container.appendChild(button)
+        }
+
+        // Create an observer instance linked to the callback function
+        const observer = new MutationObserver((mutationsList, observer) => {
+            upsertTeamsButton();
         });
 
         // Start observing the target node for configured mutations
+        upsertTeamsButton();
         observer.observe(list_inscrit_table, { childList: true, subtree: true });
         //*/
     }
